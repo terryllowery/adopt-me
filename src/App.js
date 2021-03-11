d9b08f07b5348cf09ea7216f159d2c5a37e579f1
Terry Lowery <tl@lowerytech.net>
2021-03-11 11:14:42 -0600

    init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, "Luna"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h4", {}, "Havanese"),
+  ]);
+};
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
